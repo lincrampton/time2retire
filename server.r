022441339f720ce571be592d4c83a3bf1d7f7d3e
Lin Crampton <lin.crampton@gmail.com>
2014-06-22 12:42:09 -0700

    moving help documentation around

--- server.r
+++ server.r
@@ -102,7 +102,8 @@ shinyServer(function(input, output) {
 	startCapital[ startCapital < 0 ] = NA # once nav is below 0 => run out of money
 	Retirement = startCapital / 1000000 # convert to millions
 	Retirement=cbind(ageVecYears,Retirement)
-	output$documentationText = renderText({"Adjust the slider bars to reflect the retirement scenario you wish to simulate."})
+	#output$documentationText = renderText({"Adjust the slider bars to reflect the retirement scenario you wish to simulate."})
+	output$documentationText = renderText({'... projecting retirement assets over time ...\n'})
 	output$sourceText = renderText({"Idea and original code by Pierre Chretien, updated by Michael Kapler, and then Lin Crampton. Source at https://github.com/lincrampton/time2retire. Comments/complaints to lin.crampton@gmail.com"})
 	return(Retirement)
   })
--- ui.r
+++ ui.r
@@ -5,7 +5,8 @@ shinyUI(fluidPage( #have two open parens
   titlePanel("Will I Go Broke During Retirement?"), #complete
 sidebarLayout(position="right",
 	sidebarPanel(
-		helpText('......      projecting retirement assets over time ......\n'),
+		helpText('(adjust slider bars to simulate a retirement scenario)\n'),
+		#helpText('......      projecting retirement assets over time ......\n'),
 		sliderInput("age.now", "Current Age:", min = 55, max = 110, value = 59),
 		sliderInput("years.wait", "Years Until Retirement:", min = 0, max = 40, value = 0),
 		sliderInput("n.obs", "Length of Projection (years):", min = 0, max = 40, value = 20),
